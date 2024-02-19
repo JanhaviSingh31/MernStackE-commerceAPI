3f66ba9fa62311135e8ccde0e9f2064e95fa4c24
JanhaviSingh31 <145059543+JanhaviSingh31@users.noreply.github.com>
2024-02-20 04:01:25 +0530

    Add files via upload

---
+++ Prices.js
@@ -0,0 +1,42 @@
+export const Prices =[
+    {
+        _id:0,
+        name:"Rs0 to 500",
+        array:[0,500]
+    },
+    {
+        _id:1,
+        name:"Rs500 to 1000",
+        array:[500,1000]
+    },
+    {
+        _id:1,
+        name:"Rs1000 to 1500",
+        array:[1000,1500]
+    },
+    {
+        _id:2,
+        name:"Rs1500 to 2000",
+        array:[1500,2000]
+    },
+    {
+        _id:3,
+        name:"Rs2000 to 3000",
+        array:[2000,3000]
+    },
+    {
+        _id:4,
+        name:"Rs3000 to 4000",
+        array:[3000,4000]
+    },
+    {
+        _id:1,
+        name:"Rs4000 to 5000",
+        array:[4000,5000]
+    },
+    {
+        _id:1,
+        name:"Rs5000 or more",
+        array:[5000,100000]
+    },
+]
